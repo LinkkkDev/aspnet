0916598284b02866b90114a52bc85e5708c9dedb
Tima966966 <Tima966966@gmail.com>
2023-07-23 15:08:35 +0600

    First commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run(async (context) =>
+{
+    context.Response.ContentType = "text/html; charset=utf-8";
+ 
+    // если обращение идет по адресу "/postuser", получаем данные формы
+    if (context.Request.Path == "/postuser")
+    {
+        var form = context.Request.Form;
+        string? name = form["name"];
+        string? age = form["age"];
+        await context.Response.WriteAsync($"<div><p>Name: {name}</p><p>Age: {age}</p></div>");
+    }
+    else
+    {
+        await context.Response.SendFileAsync("html/form.html");
+    }
+});
+
+app.Run();
